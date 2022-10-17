b19502b6ba331827d25d7fd55f4ce4a2ee3c36d0
Suryandini <suryandini04@gmail.com>
2022-10-17 18:21:33 +0700

    dasar

---
+++ latihan1 operator aritmatika.py
@@ -0,0 +1,16 @@
+nilaia = 10
+nilaib = 12
+ #penjumblahan
+print(nilaia + nilaib)
+ #pengurangan
+print(nilaia - nilaib)
+ #perkalian
+print(nilaia * nilaib)
+ #pembagian
+print(nilaia / nilaib)
+ #sisa bagi
+print(nilaia % nilaib)
+ #pangkat
+print(nilaia ** nilaib)
+ #pembagian bulat
+print(nilaia // nilaib)
